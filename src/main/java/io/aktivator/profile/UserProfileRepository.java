d67bd49525ce5ede7c2ec2b66bd24f0b4d31cc2e
Igor Stojanovski <igorce@outlook.com>
2019-08-01 20:58:45 +0200

    fix: Remove profile resource
    Currently there is no need for this. All the information
    can be extracted from the token. No need to duplicate
    them in the DB as well.

--- src/main/java/io/aktivator/profile/UserProfileRepository.java
+++
@@ -1,9 +0,0 @@
-package io.aktivator.profile;
-
-import org.springframework.data.repository.CrudRepository;
-
-import java.util.Optional;
-
-public interface UserProfileRepository extends CrudRepository<UserProfile, Long> {
-    Optional<UserProfile> findByUserId(String userId);
-}
