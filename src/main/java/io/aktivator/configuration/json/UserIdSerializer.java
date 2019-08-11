391ca6cf4433a7b4f0fa403c0444e6109f359705
Igor Stojanovski <igorce@outlook.com>
2019-08-11 19:55:48 +0200

    fix: UserIdSerializes should use ExternalUserService

--- src/main/java/io/aktivator/configuration/json/UserIdSerializer.java
+++ src/main/java/io/aktivator/configuration/json/UserIdSerializer.java
@@ -4,27 +4,23 @@ import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import io.aktivator.model.UserDTO;
-import io.aktivator.model.DataException;
-import io.aktivator.services.UserService;
+import io.aktivator.profile.ExternalUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.Collections;
 
 @Component
 public class UserIdSerializer extends JsonSerializer<String> {
-
     @Autowired
-    private UserService userService;
+    private ExternalUserService externalUserService;
 
     @Override
-    public void serialize(String s, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
-        UserDTO user;
-        try {
-            user = userService.getUser(s);
-            jsonGenerator.writeObject(user);
-        } catch (DataException e) {
-            jsonGenerator.writeObject("");
-        }
+    public void serialize(String ownerId, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
+        UserDTO user = externalUserService.getUser(ownerId);
+        user.setEmail("");
+        user.setAuthorities(Collections.emptyList());
+        jsonGenerator.writeObject(user);
     }
 }
