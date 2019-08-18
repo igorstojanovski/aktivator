15b47f699825b7cb1ab6558416b470fe005e9996
Igor Stojanovski <igorce@outlook.com>
2019-08-18 17:55:17 +0200

    fix: Remove unused equals method

--- src/main/java/io/aktivator/campaign/donation/DonationCampaignEntity.java
+++ src/main/java/io/aktivator/campaign/donation/DonationCampaignEntity.java
@@ -11,7 +11,6 @@ import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 import javax.validation.constraints.NotNull;
 import java.util.List;
-import java.util.Objects;
 
 @Entity
 @Getter
@@ -22,18 +21,4 @@ class DonationCampaignEntity extends Campaign {
     private Long target;
     @OneToMany(cascade = CascadeType.ALL)
     private List<ExternalPaymentMethod> externalPaymentMethods;
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof DonationCampaignEntity)) return false;
-        if (!super.equals(o)) return false;
-        DonationCampaignEntity entity = (DonationCampaignEntity) o;
-        return Objects.equals(target, entity.target);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(super.hashCode(), target);
-    }
 }
