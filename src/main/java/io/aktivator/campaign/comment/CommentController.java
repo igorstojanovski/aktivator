afb206bfdcaaa1e46651af0ad573b4d3d63da695
Igor Stojanovski <igorce@outlook.com>
2021-05-16 19:50:18 +0200

    Make the CommentDto immutable

--- src/main/java/io/aktivator/campaign/comment/CommentController.java
+++ src/main/java/io/aktivator/campaign/comment/CommentController.java
@@ -23,79 +23,73 @@ import java.util.stream.Collectors;
 @RequestMapping("/api/campaign/{campaignId}/comment")
 public class CommentController {
 
-    private final CommentService commentService;
-    private final UserService userService;
+  private final CommentService commentService;
+  private final UserService userService;
 
-    @Autowired
-    public CommentController(CommentService commentService, UserService userService) {
-        this.commentService = commentService;
-        this.userService = userService;
+  @Autowired
+  public CommentController(CommentService commentService, UserService userService) {
+    this.commentService = commentService;
+    this.userService = userService;
+  }
+
+  @PostMapping
+  public ResponseEntity<Comment> addComment(
+      @PathVariable Long campaignId, @RequestBody CommentCreateCommand commentCreateCommand) {
+    User user = userService.getCurrentUser();
+    Comment comment = new Comment();
+    comment.setCampaignId(campaignId);
+    comment.setText(commentCreateCommand.getText());
+    comment.setDate(new Date());
+    comment.setUserId(user.getId());
+    comment = commentService.createComment(comment);
+
+    return new ResponseEntity<>(comment, HttpStatus.OK);
+  }
+
+  @DeleteMapping("/{commentId}")
+  public ResponseEntity<String> deleteComment(@PathVariable Long commentId) {
+    User user = userService.getCurrentUser();
+    try {
+      if (commentService.isOwner(commentId, user.getId())) {
+        commentService.hide(commentId);
+        return new ResponseEntity<>("Comment removed.", HttpStatus.OK);
+      } else {
+        return new ResponseEntity<>("Not owner of comment.", HttpStatus.UNAUTHORIZED);
+      }
+    } catch (DataException e) {
+      return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
     }
+  }
 
-    @PostMapping
-    public ResponseEntity<Comment> addComment(@PathVariable Long campaignId,
-                                              @RequestBody CommentCreateCommand commentCreateCommand) {
-        User user = userService.getCurrentUser();
-        Comment comment = new Comment();
-        comment.setCampaignId(campaignId);
-        comment.setText(commentCreateCommand.getText());
-        comment.setDate(new Date());
-        comment.setUserId(user.getId());
-        comment = commentService.createComment(comment);
+  @GetMapping
+  public Page<CommentDto> getAllComments(
+      @PathVariable Long campaignId,
+      @SortDefault.SortDefaults({@SortDefault(sort = "id", direction = Sort.Direction.DESC)})
+          Pageable pageable) {
+    Page<Comment> comments = commentService.getComments(campaignId, pageable);
 
-        return new ResponseEntity<>(comment, HttpStatus.OK);
+    Page<CommentDto> pagedCommentDtos;
+    if (comments != null) {
+      pagedCommentDtos = getCommentDtos(comments, pageable);
+    } else {
+      pagedCommentDtos = new PageImpl<>(new ArrayList<>(), pageable, 0);
     }
+    return pagedCommentDtos;
+  }
 
-    @DeleteMapping("/{commentId}")
-    public ResponseEntity<String> deleteComment(@PathVariable Long commentId) {
-        User user = userService.getCurrentUser();
-        try {
-            if (commentService.isOwner(commentId, user.getId())) {
-                commentService.hide(commentId);
-                return new ResponseEntity<>("Comment removed.", HttpStatus.OK);
-            } else {
-                return new ResponseEntity<>("Not owner of comment.", HttpStatus.UNAUTHORIZED);
-            }
-        } catch (DataException e) {
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
-        }
-    }
+  private Page<CommentDto> getCommentDtos(Page<Comment> comments, Pageable pageable) {
+    List<CommentDto> dtos = comments.get().map(this::getCommentDto).collect(Collectors.toList());
+    return new PageImpl<>(dtos, pageable, dtos.size());
+  }
 
-    @GetMapping
-    public Page<CommentDto> getAllComments(@PathVariable Long campaignId,
-                                           @SortDefault.SortDefaults({
-                                            @SortDefault(sort = "id", direction = Sort.Direction.DESC)
-                                        }) Pageable pageable) {
-        Page<Comment> comments = commentService.getComments(campaignId, pageable);
+  private CommentDto getCommentDto(Comment comment) {
+    User internalUser =
+        userService
+            .getUser(comment.getUserId())
+            .orElseThrow(() -> new DataException("No such user found."));
+    AuthUserDTO authUser = userService.getInformationExternal(internalUser.getExternalId());
 
-        Page<CommentDto> pagedCommentDtos;
-        if(comments != null) {
-            pagedCommentDtos = getCommentDtos(comments, pageable);
-        } else {
-            pagedCommentDtos = new PageImpl<>(new ArrayList<>(), pageable, 0);
-        }
-        return pagedCommentDtos;
-    }
-
-    private Page<CommentDto> getCommentDtos(Page<Comment> comments, Pageable pageable) {
-        List<CommentDto> dtos = comments
-                .get()
-                .map(this::getCommentDto)
-                .collect(Collectors.toList());
-        return new PageImpl<>(dtos, pageable, dtos.size());
-    }
-
-    private CommentDto getCommentDto(Comment comment) {
-        CommentDto commentDto = new CommentDto();
-        commentDto.setId(comment.getId());
-        commentDto.setCampaignId(comment.getCampaignId());
-        commentDto.setDate(comment.getDate());
-        commentDto.setText(comment.getText());
-        User internalUser = userService.getUser(comment.getUserId())
-                .orElseThrow(() -> new DataException("No such user found."));
-        AuthUserDTO authUser = userService.getInformationExternal(internalUser.getExternalId());
-        commentDto.setUser(authUser);
-
-        return commentDto;
-    }
+    return new CommentDto(
+        comment.getId(), authUser, comment.getText(), comment.getCampaignId(), comment.getDate());
+  }
 }
--- src/main/java/io/aktivator/campaign/comment/CommentDto.java
+++ src/main/java/io/aktivator/campaign/comment/CommentDto.java
@@ -2,6 +2,7 @@ package io.aktivator.campaign.comment;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import io.aktivator.user.services.AuthUserDTO;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -9,13 +10,12 @@ import lombok.Setter;
 import java.util.Date;
 
 @Getter
-@Setter
-@NoArgsConstructor
+@AllArgsConstructor
 public class CommentDto {
-    private Long id;
-    private AuthUserDTO user;
-    private String text;
-    private Long campaignId;
+    private final Long id;
+    private final AuthUserDTO user;
+    private final String text;
+    private final Long campaignId;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
-    private Date date;
+    private final Date date;
 }
