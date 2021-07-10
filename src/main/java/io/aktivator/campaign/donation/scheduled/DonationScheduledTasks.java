15e4aac50ee2835e0a44ac0b177862325ded4bd0
Igor Stojanovski <igorce@outlook.com>
2021-07-10 23:47:26 +0200

    Replace system out with logger

--- src/main/java/io/aktivator/campaign/donation/scheduled/DonationScheduledTasks.java
+++ src/main/java/io/aktivator/campaign/donation/scheduled/DonationScheduledTasks.java
@@ -3,6 +3,8 @@ package io.aktivator.campaign.donation.scheduled;
 import io.aktivator.campaign.CampaignStatus;
 import io.aktivator.campaign.donation.Donation;
 import io.aktivator.campaign.donation.DonationService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -13,7 +15,7 @@ import java.util.stream.Collectors;
 
 @Component
 public class DonationScheduledTasks {
-
+  private static final Logger LOGGER = LoggerFactory.getLogger(DonationScheduledTasks.class);
   @Autowired private DonationService donationService;
 
   @Scheduled(fixedDelay = 60000)
@@ -25,28 +27,27 @@ public class DonationScheduledTasks {
             .peek(d -> d.setCampaignStatus(CampaignStatus.ACTIVE))
             .collect(Collectors.toList());
     for (Donation donation : updatedDonations) {
-      System.out.println("Will set to active: " + donation.getId());
+      LOGGER.debug("Will set to active: {}", donation.getId());
     }
-    if(!updatedDonations.isEmpty()) {
+    if (!updatedDonations.isEmpty()) {
       donationService.saveAll(updatedDonations);
       autoExpireDonationCampaigns();
     }
-
   }
 
   @Scheduled(fixedDelay = 60000)
   public void autoExpireDonationCampaigns() {
     List<Donation> newDonations = donationService.getDonations(CampaignStatus.ACTIVE);
     List<Donation> updatedDonations =
-            newDonations.stream()
-                    .filter(d -> d.getEndDate().before(new Date()))
-                    .peek(d -> d.setCampaignStatus(CampaignStatus.EXPIRED))
-                    .collect(Collectors.toList());
+        newDonations.stream()
+            .filter(d -> d.getEndDate().before(new Date()))
+            .peek(d -> d.setCampaignStatus(CampaignStatus.EXPIRED))
+            .collect(Collectors.toList());
     for (Donation donation : updatedDonations) {
-      System.out.println("Will set to expired: " + donation.getId());
+      LOGGER.debug("Will set to expired: {}", donation.getId());
     }
 
-    if(!updatedDonations.isEmpty()) {
+    if (!updatedDonations.isEmpty()) {
       donationService.saveAll(updatedDonations);
     }
   }
